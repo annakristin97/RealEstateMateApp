893d26a84505a6f69eb83e8b3904711359158081
Númi Steinn Baldursson <numisteinn@gmail.com>
2020-04-15 09:40:45 +0000

    login

--- Stormy-master/app/src/main/java/is/siggigauti/stormy/ui/LoginActivity.java
+++ Stormy-master/app/src/main/java/is/siggigauti/stormy/ui/LoginActivity.java
@@ -1,20 +1,33 @@
 package is.siggigauti.stormy.ui;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import is.siggigauti.stormy.R;
+import is.siggigauti.stormy.weather.User;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText userName;
-    private EditText userPassword;
 
     private MainActivity mainActivity;
 
@@ -23,6 +36,11 @@ public class LoginActivity extends AppCompatActivity {
     Button signUpButton;
     @BindView((R.id.loginButton))
     Button loginButton;
+    @BindView((R.id.userName_input))
+    EditText userNameInput;
+    @BindView((R.id.userPassword_input))
+    EditText userPasswordInput;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -36,17 +54,89 @@ public class LoginActivity extends AppCompatActivity {
                 openSignUpPage();
             }
         });
-        userName = (EditText) findViewById(R.id.userName_input);
-        userPassword = (EditText) findViewById(R.id.userPassword_input);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = userNameInput.getText().toString();
+                loginUser(userNameInput.getText().toString(), userPasswordInput.getText().toString());
+            }
+        });
+//        userName = (EditText) findViewById(R.id.userName_input);
+//        userPassword = (EditText) findViewById(R.id.userPassword_input);
 
     }
 
+    private void loginUser(String userName, String password) {
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Logging In!");
+        progressDialog.show();
+        String URL_DATA = "http://10.0.2.2:9090/login";
+        RequestBody formBody = new FormBody.Builder()
+                .add("username", userName)
+                .add("emailaddress", "")
+                .add("password", password)
+                .build();
+
+        Request request = new Request.Builder()
+                .url(URL_DATA)
+                .post(formBody)
+                .build();
+        callBackend(request);
+    }
+
+    private void callBackend(Request request){
+        OkHttpClient client = new OkHttpClient();
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                System.out.println("bilad");
+                Log.e("IO Exception/Failure", "Exception caught: ", e);
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    String userData = response.body().string();
+                    System.out.println(userData);
+                    User user = parseUserData(userData);
+                    Log.v("Error", userData);
+                    if (response.isSuccessful()) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                goToMain();
+                            }
+                        });
+                    } else {
+                        System.out.println("Error!");
+                    }
+                } catch (IOException e) {
+                    Log.e("IO Exception", "Exception caught: ", e);
+                }catch (JSONException e) {
+                    Log.e("JSON Exception", "JSON caught: ", e);
+                }
+            }
+        });
+    }
+
+    private User parseUserData(String userData) throws JSONException {
+        JSONObject jsonObk= new JSONObject(userData);
+        JSONObject json = jsonObk.getJSONObject("user");
+        return new User(json.get("userName").toString(),
+                json.get("userPassword").toString(),
+                json.get("userEmail").toString());
+    }
 
     private void openSignUpPage() {
-        System.out.println("sjomli þú ýttir á signup fragmentos");
+        System.out.println("sjomli þú ýttir á signup");
         Intent intent = new Intent(this, SignUpActivity.class);
         startActivity(intent);
         //mainActivity.getUsers();
 
+    }
+    private void goToMain(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
